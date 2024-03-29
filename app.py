777c355cd6bc2c3c606bc9334a4fc910d7c5e2f5
kdokan <kotarodokan1219@gmail.com>
2024-03-29 07:53:26 +0000

    可視化部分を追加

--- app.py
+++ app.py
@@ -34,13 +34,13 @@ if upload_file is not None:
     data_cor_top_3 = data_cor.head(2).index.tolist()
     data_cor_top3_for_plot = data[["ds", y] + data_cor_top_3]
     
-    usecols = st.multiselect(
-            '比較したいカラムを2つ入れてください',
-            list(data_cor_top3_for_plot.drop(columns="ds").columns),
-            list(data_cor_top3_for_plot.drop(columns="ds").columns))
+    #usecols = st.multiselect(
+    #        '比較したいカラムを2つ入れてください',
+    #        list(data_cor_top3_for_plot.drop(columns="ds").columns),
+    #        list(data_cor_top3_for_plot.drop(columns="ds").columns))
 
     # グラフの描画
-    fig = px.line(data_cor_top3_for_plot, x='ds', y=usecols, title='時系列PLOT')
+    fig = px.line(data_cor_top3_for_plot, x='ds', y=[y] + data_cor_top_3, title='目的変数と相関上位2変数のPLOT')
     fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
 
     # プロットをStreamlitで表示
--- develop.py
+++ develop.py
@@ -2,7 +2,7 @@
 # デモデータ作成
 import pandas as pd
 
-
+#%%
 def clening_google_trend(path_data):
     # データ読み込み
     data = pd.read_csv(
@@ -58,3 +58,37 @@ data_cor_top3_for_plot.drop("ds")
 data_cor = data_cor.corr()
 #data_cor = data_cor[data_cor[y] != y]
 # %%
+data = pd.read_excel(
+        "data/since2003_visitor_arrivals_February_2024.xlsx"
+        , sheet_name = "2024"
+        )
+
+data
+
+
+
+
+
+
+
+
+
+# %%
+from openpyxl import load_workbook
+
+# Excelファイルを読み込む
+workbook = load_workbook(filename='ファイル名.xlsx')
+
+# シートを取得
+sheet = workbook['シート名']
+
+# 範囲を指定してセルの値を取得
+data = []
+for row in sheet.iter_rows(min_row=2, max_row=10, min_col=1, max_col=3):  # 例: 2行目から10行目まで、1列目から3列目までの範囲
+    row_data = []
+    for cell in row:
+        row_data.append(cell.value)
+    data.append(row_data)
+
+# 取得したデータを表示
+print(data)
